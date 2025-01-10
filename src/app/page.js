889c39f4a3aad7cb43ea11ab8c773e9c023c13bc
birukGetachew <burab1742@gmail.com>
2025-01-10 12:24:27 -0800

    df

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,64 @@
 "use client";
-
-import dynamic from "next/dynamic";
-
-const HomeContent = dynamic(() => import("./Home"), {
-  ssr: false,
-});
+import { useState, useEffect } from "react";
+import Text from "./components/text";
+import Navbar from "./components/Nav";
+import About from "./components/About";
+import Services from "./components/Service";
+import Team from "./components/Team";
+import Project from "./components/Project";
+import Partner from "./components/Partner";
+import Fotter from "./components/Fotter";
 
 export default function Home() {
-  return <HomeContent />;
+  const [visibleSections, setVisibleSections] = useState({});
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Select all sections on the page
+      const sections = document.querySelectorAll("section");
+
+      // Create IntersectionObserver to monitor visibility
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const newVisibleSections = { ...visibleSections }; // Copy the current visibleSections state
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              // Mark the section as visible
+              newVisibleSections[entry.target.id] = true;
+            } else {
+              // Mark the section as not visible
+              newVisibleSections[entry.target.id] = false;
+            }
+          });
+          setVisibleSections(newVisibleSections); // Update the state with visibility info
+        },
+        { threshold: [0.2, 1] } // Monitor both when 20% and 100% of the section is visible
+      );
+
+      // Observe all sections
+      sections.forEach((section) => observer.observe(section));
+
+      // Cleanup observer on component unmount
+      return () => observer.disconnect();
+    }
+  }, [visibleSections]);
+
+  return (
+    <div className="w-screen">
+      <img src="/grid.svg" alt="grid" style={{ height: "100vh" }} />
+      <div className="absolute top-0 w-screen">
+        <Navbar />
+        <Text />
+      </div>
+
+      <div className="relative sm:mt-20 md:mt-20">
+        <About isVisible={visibleSections.about} />
+      </div>
+      <Services isVisible={visibleSections.services} />
+      <Team isVisible={visibleSections.team} />
+      <Project isVisible={visibleSections.project} />
+      <Partner isVisible={visibleSections.partner} />
+      <Fotter isVisible={visibleSections.fotter} />
+    </div>
+  );
 }
